e31972a275b031a7e2a803b3c6c1041a9d43411e
omkar <pendkalkar.omkar786@gmail.com>
2023-06-30 14:56:45 +0530

    popular recipelist done, bottom navigation remaining

--- lib/components/popular_recipe_list.dart
+++ lib/components/popular_recipe_list.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:recipes/models/recipe.dart';
 
-class PopularRecipeList extends StatefulWidget{
+class PopularRecipeList extends StatefulWidget {
   const PopularRecipeList({super.key});
 
   @override
   State<StatefulWidget> createState() => _PopularRecipeListState();
-  }
+}
 
 class _PopularRecipeListState extends State<PopularRecipeList> {
   @override
@@ -17,18 +18,82 @@ class _PopularRecipeListState extends State<PopularRecipeList> {
         physics: const NeverScrollableScrollPhysics(),
         itemCount: recipes.length,
         shrinkWrap: true,
-        itemBuilder: (context, index){
+        itemBuilder: (context, index) {
           return Container(
             height: 100,
-            margin:const EdgeInsets.only(bottom: 16),
-            padding:const EdgeInsets.all(16),
+            margin: const EdgeInsets.only(bottom: 16),
+            padding: const EdgeInsets.all(16),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(15),
               color: Colors.transparent,
             ),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                
+                // Expanded(
+                   Container(
+                    width: 75,
+                    height: 55,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image:
+                            AssetImage('images/${recipes[index].recipeImage}'),
+                      ),
+                    ),
+                  ),
+                // ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment:  MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        recipes[index].recipeName,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: const TextStyle(
+                            fontSize: 17, fontWeight: FontWeight.w700),
+                      ),
+                      Text(
+                        '${recipes[index].recipeMaker}\'s recipe ',
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: Color(0xff9a9db0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    CircleAvatar(
+                      radius: 15,
+                      backgroundColor: recipes[index].endColor,
+                      child: Text(
+                        recipes[index].recipeMaker[0],
+                        style: TextStyle(
+                          color: recipes[index].startColor,
+                        ),
+                      ),
+                    ),
+                    // const Spacer(),
+                    Row(
+                      children: [
+                        SvgPicture.asset('svg/ic_share.svg'),
+                        const SizedBox(width: 5,),
+                        SvgPicture.asset('svg/ic_save.svg'),
+                      ],
+                    )
+                  ],
+                ),
               ],
             ),
           );
@@ -36,7 +101,4 @@ class _PopularRecipeListState extends State<PopularRecipeList> {
       ),
     );
   }
-
 }
-
-
--- lib/models/recipe.dart
+++ lib/models/recipe.dart
@@ -20,42 +20,43 @@ class Recipe {
       recipeName: "Chicken Biryani",
       recipeImage: "chicken-biryani.jpeg",
       recipeMaker: "Omkar Pendkalkar",
-      startColor: const Color(0xFFF37552),
+        startColor: const Color(0xFF621e14),
       endColor: const Color(0xFFF58B5A)
     ),
     Recipe(
         recipeName: "Pav Bhaji",
         recipeImage: "pav-bhaji.jpg",
         recipeMaker: "Omkar Pendkalkar",
-        startColor: const Color(0xFFF37552),
-        endColor: const Color(0xFFF58B5A)
+        startColor: const Color(0xFF621e14),
+        endColor: const Color(0xFFd13b10)
     ),
     Recipe(
         recipeName: "Onion Chizz Pizza",
         recipeImage: "pizza.jpg",
         recipeMaker: "Omkar Pendkalkar",
-        startColor: const Color(0xFFF37552),
-        endColor: const Color(0xFFF58B5A)
+        startColor: const Color(0xFF621e14),
+        endColor: const Color(0xFFd13b10)
     ),
     Recipe(
         recipeName: "Corn Sabji",
         recipeImage: "corn-sabji.jpeg",
         recipeMaker: "Omkar Pendkalkar",
-        startColor: const Color(0xFFF37552),
-        endColor: const Color(0xFFF58B5A)
+        startColor: const Color(0xFF621e14),
+        endColor: const Color(0xFFd13b10)
+
     ),
     Recipe(
         recipeName: "Full Meal Thali",
         recipeImage: "full-thali.jpg",
         recipeMaker: "Omkar Pendkalkar",
-        startColor: const Color(0xFFF37552),
-        endColor: const Color(0xFFF58B5A)
+        startColor: const Color(0xFF621e14),
+        endColor: const Color(0xff724a0d)
     ),
     Recipe(
         recipeName: "Vegetable Salad",
         recipeImage: "salad.jpg",
         recipeMaker: "Omkar Pendkalkar",
-        startColor: const Color(0xFFF37552),
-        endColor: const Color(0xFFF58B5A)
+        startColor: const Color(0xFF8b1d07),
+        endColor: const Color(0xFFee882d)
     ),
   ];
